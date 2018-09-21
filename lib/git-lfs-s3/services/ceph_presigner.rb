e834bc1cfb51a95eae16ae92578e8ae9617160b6
Damien Merenne <dam@cosinux.org>
2018-09-21 16:25:14 +0200

    Use aws sdk for generating signature.

--- lib/git-lfs-s3/services/ceph_presigner.rb
+++ lib/git-lfs-s3/services/ceph_presigner.rb
@@ -1,24 +1,21 @@
-require 'base64'
-require 'date'
-require 'digest/sha1'
-require 'openssl'
-require 'uri'
+# frozen_string_literal: true
+
+require 'aws-sdk'
 
 module GitLfsS3
   module CephPresignerService
     extend self
     extend AwsHelpers
-    
+
     def signed_url(obj)
-      expire_at = (DateTime.now + 1).strftime("%s")
-      secret_access_key = GitLfsS3::Application.settings.aws_secret_access_key
-      access_key_id = GitLfsS3::Application.settings.aws_access_key_id
-      endpoint = GitLfsS3::Application.settings.endpoint
-      digest = OpenSSL::Digest.new('sha1')
-      can_string = "PUT\n\napplication/octet-stream\n#{expire_at}\n/#{obj.bucket_name}/#{obj.key}"
-      hmac = OpenSSL::HMAC.digest(digest, secret_access_key, can_string)
-      signature = URI.escape(Base64.encode64(hmac).strip, /[\+=?@$&,\/:;\?]/)
-      "#{endpoint}/#{obj.bucket_name}/#{obj.key}?Signature=#{signature}&AWSAccessKeyId=#{access_key_id}&Expires=#{expire_at}"
+      @@s3 ||= Aws::S3::Resource.new(region: GitLfsS3::Application.settings.region,
+                                     access_key_id: GitLfsS3::Application.settings.aws_access_key_id,
+                                     secret_access_key: GitLfsS3::Application.settings.aws_secret_access_key)
+      bucket = s3.bucket(obj.bucket_name).object(obj.key)
+      bucket.presigned_url(:put, expires_in: 2.hours.from_now, acl: 'private').
+        tap do |url|
+        Rails.logger.debug('using ' +url)
+      end
     end
   end
 end
